b58d4851bc420711cba45a9daa6033e55b3a8a2c
hedda0122 <113341428+hedda0122@users.noreply.github.com>
2022-10-27 19:22:15 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-# ITE-301-Constellations
-ITE 301- Constellations
-John Ken Talusan
-Ernest Mikhail Santos
-Laurence Jade Javier
-Submitted 27/10/2022
+# newfile
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\test\\newfile" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Dev\\flutter"
+  "PROJECT_DIR=D:\\test\\newfile"
+  "FLUTTER_ROOT=D:\\Dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\test\\newfile\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\test\\newfile"
+  "FLUTTER_TARGET=D:\\test\\newfile\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\test\\newfile\\.dart_tool\\package_config.json"
+)
