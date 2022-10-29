c926525fe4ef64049b40bc29db1be2d5f42cbbd9
hedda0122 <113341428+hedda0122@users.noreply.github.com>
2022-10-29 09:37:25 +0800

    Update details.dart

--- details.dart
+++ details.dart
@@ -4,9 +4,8 @@
 /// Laurence Jade Javier
 /// Ernest Sacdal
 /// John Ken Talusan
+///Leslie Kate Pagaduan
 /// October 28 2022
-/// 
-
 import 'package:flutter/material.dart';
 
 class DetailsPage extends StatelessWidget {
